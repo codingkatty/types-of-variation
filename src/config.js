4e7f07926177a0aeb9a1ce0f1c02d15c2a6387c0
Lim Xin Ying <m001720@permatapintar.ukm.edu.my>
2024-12-01 15:16:19 +0800

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,1 +1,2 @@
+// Temp only, because I can't figure out how to access environment secrets.
 export const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1zZnV0Z2pnZmxna2NreHJla3NwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzMwMTk1NDEsImV4cCI6MjA0ODU5NTU0MX0.pIQlsN43fHNmB1MCjONz6jomwz3x3vdgfGyfSlmWw_U'
